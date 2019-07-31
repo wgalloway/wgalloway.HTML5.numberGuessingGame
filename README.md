2c9c7f46df977073b2eaf9b817639ee2227010cd
Weslee Galloway <wesleegalloway@gmail.com>
2019-07-30 22:00:55 -0400

    Adding all files

---
+++ README.md
@@ -0,0 +1,1 @@
+# wgalloway.blog.me
---
+++ main.js
@@ -0,0 +1,86 @@
+// Game Variables
+var mysteryNum = Math.floor(Math.random() * 100);
+var playerGuess;
+var remainGuesses = 10;
+var guessMade = 0;
+var gameSate = "";
+var gameWon = false;
+
+// Select input and output HTML id elements
+var input = document.querySelector('#input');
+var output = document.querySelector('#output');
+
+// Select button variable HTML element
+var button = document.querySelector('button');
+button.style.cursor = "pointer";
+// Add a event lister to button
+button.addEventListener("click", clickHandler, false);
+
+// Adding enter key listener
+window.addEventListener("keydown", keydownHandler, false);
+function keydownHandler(event){
+  // Enter key code is 13
+  if(event.keyCode === 13) {
+    validateGuess();
+  }
+}
+
+function clickHandler(){
+  validateGuess();
+}
+
+function validateGuess() {
+  playerGuess = parseInt(input.value);
+
+  if(isNaN(playerGuess)) {
+    output.innerHTML = "Please enter a number.";
+  } else {
+    playGame();
+  }
+}
+
+function endGame(){
+  if(gameWon){
+    output.innerHTML
+	  = "Yes, it's " + mysteryNum + "!" + "<br>"
+	  + "It only took you " + guessMade + " guesses.";
+  } else {
+    output.innerHTML
+	  = "No more guesses left!" + "<br>"
+	  + "The number was: " + mysteryNum + ".";
+  }
+  
+  // Disable button and remove clickHandler
+  button.removeEventListener("click", clickHandler, false);
+  button.disabled = true;
+
+  // Remove keyHandler
+  window.removeEventListener("keydown", keydownHandler, false);
+
+  // Disable the input field
+  input.disabled = true;
+}
+
+function playGame(){
+  remainGuesses--;
+  guessMade++;
+  gameState = " Guess: " + guessMade + ", Remaining: " + remainGuesses;
+
+  // Grab value from input field
+  playerGuess = parseInt(input.value);
+  
+  if(playerGuess > mysteryNum){
+    output.innerHTML = "Guess is too high." + gameState;
+    if(remainGuesses < 1) {
+      endGame()
+    }
+  } else if(playerGuess < mysteryNum) {
+    output.innerHTML = "Guess is too low." + gameState;
+    if(remainGuesses < 1) {
+      endGame()
+    }
+  } else if(playerGuess === mysteryNum){
+    gameWon = true;
+    endGame()
+  }
+}
